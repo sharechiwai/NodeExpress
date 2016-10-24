8df2f8e8564e852fd89550fba1adcd2da608701c
Chi <yau.chiwai@gmail.com>
2016-10-24 18:39:14 +0100

    close #4 tried env variable

--- src/routes/tutorialRoutes.js
+++ src/routes/tutorialRoutes.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var dotenv = require('dotenv');
+dotenv.load();
 
 var tutorialRouter = express.Router();
 
@@ -18,10 +20,10 @@ var router = function(nav, config) {
             res.render('tutorial/index', {
                 title: 'ShareChiWai Tutorial',
                 nav: nav,
-                content: 'enviroment_variable'
+                content: 'environment_variable',
+                variable: process.env.var1
             });
         });
-
 
     return tutorialRouter;
 };
